92a4a97ee6f01ad7f58eadf80deeedc8df0254bc
Mikołaj Pniak <mikolaj.pniak@schibsted.com>
2022-12-03 08:59:22 +0100

    day 3

--- src/com/github/crunchynomnom/aoc2022/puzzles/Day03.kt
+++ src/com/github/crunchynomnom/aoc2022/puzzles/Day03.kt
@@ -4,12 +4,46 @@ import Puzzle
 
 class Day03 : Puzzle() {
     override fun part1(input: List<String>) {
-        TODO("Not yet implemented")
+        println(input.sumOf { getPrio(findMatch(it)) })
     }
 
     override fun part2(input: List<String>) {
-        TODO("Not yet implemented")
+        println(input.chunked(3).sumOf { getPrio(getCommonFor3(it)) })
     }
 
+    // returns common letter
+    private fun findMatch(backpack: String): Char {
+        val existsOnLeft = BooleanArray(53) { _ -> false}
+        for (i in 0 until backpack.length/2) {
+            existsOnLeft[getPrio(backpack[i])] = true
+        }
+        for (i in 0 until backpack.length/2) {
+            if (existsOnLeft[getPrio(backpack[backpack.length/2 + i])]) {
+                return backpack[backpack.length/2 + i]
+            }
+        }
+        return '!'
+    }
+
+    private fun getPrio(c: Char): Int {
+        if (c.isLowerCase()) return c.minus('`')
+        return c.minus('&')
+    }
+
+    private fun getCommonFor3(backpacks: List<String>): Char {
+        val b = backpacks.sortedBy { it.length }
+        val existsOnFirst = BooleanArray(53) { _ -> false}
+        val existsOnSecond = BooleanArray(53) { _ -> false}
+        for (l in b[0])
+            existsOnFirst[getPrio(l)] = true
+        for (l in b[1])
+            existsOnSecond[getPrio(l)] = true
+        for (l in b[2]) {
+            val ll = getPrio(l)
+            if (existsOnFirst[ll] and existsOnSecond[ll])
+                return l
+        }
+        return '!'
+    }
 }
 
